00d18bcc2f166c9c65ec1c7bf2accdd1d4f0dc98
TyuUWO <tyu263@uwo.ca>
2021-10-10 15:42:59 -0400

    Q10 in commented code

--- Lab.cpp
+++ Lab.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+//#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -13,7 +14,14 @@ int main(){
 		cout<<"Type something below. Type \"Done\" to exit." << endl;
 		cin>> userData;
 		
-		
+		/*
+		//this code goes at the bottom right above terminateChildren()
+		std::transform(userData.begin(), userData.end(), userData.begin(), ::tolower);
+		std::size_t found = outputLine.find("found");
+		if (found != string::npos){
+			break;
+	}
+		*/
 		//write a line to terminate child threads here
 	}
 	return 0;
