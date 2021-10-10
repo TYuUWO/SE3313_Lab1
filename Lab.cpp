b75efa09614bb581b7967d3c633eed03691698a0
TyuUWO <tyu263@uwo.ca>
2021-10-10 14:54:45 -0400

    test

--- Lab.cpp
+++ Lab.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main(){
+	string userData;
+	while (userData != "Done"){
+		cout<<"Type something below. Type \"Done\" to exit." << endl;
+		cin>> userData;
+		
+		
+		//write a line to terminate child threads here
+	}
+	return 0;
+}
