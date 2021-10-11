28f5810d646ee0ac6de63b9b02b9a55143091f54
TyuUWO <tyu263@uwo.ca>
2021-10-11 13:32:28 -0400

    mostly done

--- Lab.cpp
+++ Lab.cpp
@@ -4,8 +4,13 @@
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
+#include <vector>
+#include <signal.h>
 
 using namespace std;
+namespace{
+	volatile sig_atomic_t shutdownSig = 1;
+}
 int createFile(string fileName) { //This should just create the file
 	std::ofstream MyFile("file" + std::to_string(getpid()) + ".txt");
 	MyFile << fileName << endl;
@@ -13,14 +18,15 @@ int createFile(string fileName) { //This should just create the file
 	return 0;
 }
 
-int writeToFile(string userData) {
-	sleep(1);
-	//Can you put code that writes to a file here 
-	return 0;
+void Exit(int signalNumber){
+	shutdownSig = 0;
 }
 
-pid_t createChild(){
+pid_t createChild(string userData){
 	pid_t childPid = fork();
+	if (userData == "Done"){
+		exit(EXIT_SUCCESS);
+	}
 	if(childPid < 0){
 		perror("Fork Failed");
 		exit(EXIT_FAILURE);
@@ -31,6 +37,17 @@ pid_t createChild(){
 	}
 	else{
 		cout << "printed from child process " << getpid() << endl;
+		createFile("FileNo" + getpid());
+		string fileName = ("file" + to_string(getpid())+".txt");
+		ofstream childFile;
+		childFile.open(fileName);
+		while (shutdownSig){
+			signal(SIGTERM, Exit);
+			cout<< "writing: " + userData <<endl;
+			sleep(1);
+			childFile << userData + "\n";
+		}
+		childFile.close();
         	exit(EXIT_SUCCESS);
 	}
 }
@@ -38,15 +55,20 @@ pid_t createChild(){
 int main() {
 	string userData;
 	int i = 0;
-	int childern [5] = {};
+	vector<int> children;
+	children.reserve(5);
 	while(userData != "Done"){ //If its finite it would terminate at 5
 		i++;
 		cout << "Type something below. Type \"Done\" to exit."<< endl;
 		cin >> userData;
-		childern [i -1] = createChild();
-		printf("Child with ForkID %d\n",childern [i-1]);
+		cout << "this is the parent thread \n";
+		children.push_back(createChild(userData));
+		printf("Child with ForkID %d\n",children [i-1]);
 		cout << i;
-		createFile("FileNo" + i);
+		//createFile("FileNo" + i);
 		}
+	for (int j=0; j<children.size(); j++){
+		kill(children[j], SIGTERM);
+	}
 	return EXIT_SUCCESS;
 	}
