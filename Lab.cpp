f1ee87e5b589d49a4a80208a3c036d0979d48bf4
TYuUWO <69701775+TYuUWO@users.noreply.github.com>
2021-10-11 10:49:26 -0400

    Update Lab.cpp
    writeToFile moved to ChildThread

--- Lab.cpp
+++ Lab.cpp
@@ -14,14 +14,8 @@ int createFile(string fileName) { //This should just create the file
 	return 0;
 }
 
-int writeToFile(string userData) {
-	sleep(1);
-	//Can you put code that writes to a file here 
-	return 0;
-}
-
 pid_t createChild(){
-	pid_t childPid = fork();
+	pid_t childPid = fork(const char* program, char** arg_list);
 	if(childPid < 0){
 		perror("Fork Failed");
 		exit(EXIT_FAILURE);
@@ -37,23 +31,28 @@ pid_t createChild(){
 }
 
 int main() {
-	string program_name ("ChildThread");
+	string program_name ("ChildThread.cpp");
+	string fileName;
+	string userData;
+	char *arg_list[] = {program_name.data(), fileName, userData, nullptr};
 	if (!exists(program_name)){
         	cout << "Program file 'child' does not exist in current directory!\n";
         	exit(EXIT_FAILURE);
     	}
-	string userData;
 	int i = 0;
 	vector<int> children;
     	children.reserve(5); //start with a vector of size 5; size will increase when needed on its own
 	while(userData != "Done"){ //If its finite it would terminate at 5
 		cout << "Type something below. Type \"Done\" to exit."<< endl;
 		cin >> userData;
-		childern [i] = createChild();
+		childern.push_back(createChild(program_name.c_str, arg_list));
 		printf("Child with ForkID %d\n", childern[i]);
 		createFile("FileNo" + i);
 		i++;
 		printf("There is %d array element(s)",i);
 		}
+	for (int i = 0; i < children.size(); ++i) {
+        	kill(children[i], SIGTERM); //kill all child threads
+   	}
 	return EXIT_SUCCESS;
 	}
