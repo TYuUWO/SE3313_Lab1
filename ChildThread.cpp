d2047be5453543cb499fa35597a46b2dffe605cd
TYuUWO <69701775+TYuUWO@users.noreply.github.com>
2021-10-11 10:16:59 -0400

    Update ChildThread.cpp

--- ChildThread.cpp
+++ ChildThread.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <vector>
+#include <chrono>
+#include <thread>
 
+using namespace std;
+
+int writeToFile(string userData, string fileName) {
+	sleep(1);
+  cout<<"writing to file..."<<endl;
+	//Can you put code that writes to a file here 
+	return 0;
+}
+int main(string userData, string fileName){
+  while(true){
+    writeToFile(userData, fileName)
+  }
+}
